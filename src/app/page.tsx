77a31bebc7cb56fbedd32f0263de77bcda9aeab9
[yuvraj sain] <mryuvrajsain@gmail.com>
2026-07-25 18:06:12 +0530

    Remove GitHub activity section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Skills } from "@/components/Skills"
 import { Projects } from "@/components/Projects"
 import { Experience } from "@/components/Experience"
 import { Education } from "@/components/Education"
-import { GithubStats } from "@/components/GithubStats"
 import { Contact } from "@/components/Contact"
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
       <Projects />
       <Experience />
       <Education />
-      <GithubStats />
       <Contact />
       
       <footer className="py-8 border-t border-border/50 bg-background text-center">
